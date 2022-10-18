19386a9fa6f303bb31bf072c83244aeefcfff2c4
Ramiro Renteria <Ramis28@yahoo.com>
2022-10-17 23:51:39 -0700

    First commit

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+        <div className="container-multiple">
+            <img src="./Group77.png" className="group-pic" alt="people doing various activities"/>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NavBar() {
+    return(
+        <nav className="container-nav">
+            <img src="./airbnb1.png" className="nav-edit" alt="AirBnb red logo"/>
+        </nav>
+
+    )
+}
